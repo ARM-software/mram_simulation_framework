d96f75cdd3cd552cae2cf0e71048dd6fa8be766c
Fernando Garcia Redondo <fernando.garciaredondo@arm.com>
2021-07-28 09:42:54 +0100

    sllgs_importer returning histogram 2d bins
    mend
    
    mend

--- src/sllgs_fpe_comparison/sllgs_importer.py
+++ src/sllgs_fpe_comparison/sllgs_importer.py
@@ -27,6 +27,7 @@ def process_sllgs_data(data_file, time_file, _t_delay, dim_points):
     # ~/SharePoint/ProjectEnergyScalableMRAM/docs/essderc_2021/raw_data
     ####################################################################
     data_tol = np.abs(data_tol)
+    print(f'max theta in data_tol {np.max(data_tol)}')
     print(data_tol.shape)
     x = np.zeros(data_tol.shape[0]*data_tol.shape[1])
     y = np.zeros(data_tol.shape[0]*data_tol.shape[1])
@@ -37,15 +38,16 @@ def process_sllgs_data(data_file, time_file, _t_delay, dim_points):
             x[idx] = _time_tol[j]
             y[idx] = np.abs(data_tol[i, j])
     _s_llgs_bins = [np.max([50, data_tol.shape[1]]), dim_points]
-    _sllgs_rho, _, _ = np.histogram2d(x, y,
-                                      bins=_s_llgs_bins,
-                                      density=True)
-    print(_sllgs_rho.shape)
+    _sllgs_rho = np.histogram2d(x, y,
+                                range=[None, [0, np.pi]],
+                                bins=_s_llgs_bins,
+                                density=True)
+    print(f'histogram shape: {_sllgs_rho[0].shape}')
     # normalize and log
     _sllgs_tol_sw_idx = 0
-    for h_idx, h in enumerate(_sllgs_rho):
+    for h_idx, h in enumerate(_sllgs_rho[0]):
         area = np.abs(np.trapz(h*np.sin(theta_axis), x=theta_axis))
-        _sllgs_rho[h_idx] = h/(2*np.pi*area)
+        _sllgs_rho[0][h_idx] = h/(2*np.pi*area)
 
     _sllgs_ps_tol = np.zeros(_time_tol.shape[0])
     _sllgs_tol_sw_idx = 0
@@ -56,15 +58,17 @@ def process_sllgs_data(data_file, time_file, _t_delay, dim_points):
             _sllgs_tol_sw_idx = j
             print('switch at ', j)
 
-    _sllgs_rho = _sllgs_rho.T
-    print(f'H_tol shape: {_sllgs_rho.shape}')
     # H_tol = np.log(H_tol.T)
     print('H computed, sw at {1e9*_time_tol[sllgs_low_tol_switching_idx]}')
     rho_0_time_idx = np.searchsorted(_time_tol, _t_delay)
     print(f'fitting time: {_time_tol[rho_0_time_idx]}')
     # shifting till delay
     _time_tol = _time_tol[rho_0_time_idx:] - _t_delay
-    _sllgs_rho = _sllgs_rho[:, rho_0_time_idx:]
+    # re-make the tupple
+    _sllgs_rho = ((_sllgs_rho[0].T)[:, rho_0_time_idx:],
+                  _sllgs_rho[1],
+                  _sllgs_rho[2])
+    print(f'H_tol shape: {_sllgs_rho[0].shape}')
     _sllgs_tol_sw_idx -= rho_0_time_idx
     _sllgs_ps_tol = _sllgs_ps_tol[rho_0_time_idx:]
     return _time_tol, _sllgs_rho, _sllgs_tol_sw_idx, _sllgs_ps_tol
